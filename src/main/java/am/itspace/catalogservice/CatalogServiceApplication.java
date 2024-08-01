74f3fa0877d9319c082d6775101e53e72d72c5d5
Hovik Yeghikyan <hovikyeghikyan@gmail.com>
2024-08-01 22:27:59 +0300

    new commit

--- src/main/java/am/itspace/catalogservice/CatalogServiceApplication.java
+++ src/main/java/am/itspace/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,11 @@ package am.itspace.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
